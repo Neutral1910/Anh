8a4ed8efe7504498c5ed6e369eac992129f21884
Gero Zickermann <hvv@gmx.de>
2021-09-21 16:15:24 +0200

    ttt

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World5599999999999999luuffuuuuuuuuuuuuuuuuuuuuuuuuul99999,,9999 ");
+		System.out.println("Hello World559999hhh9999999999luuffuuuuuuuuuuuuuuuuuuuuuuuuul99999,,9999 ");
 
 	}
 
