121ef742ee4a90b19e50fc1b22fe5b4f7e6be34a
Gero Zickermann <hvv@gmx.de>
2021-09-21 16:14:30 +0200

    ff

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World5599999999999999luuuuuuuuuuuuuuuuuuuuuuuuuuul99999,,9999 ");
+		System.out.println("Hello World5599999999999999luuffuuuuuuuuuuuuuuuuuuuuuuuuul99999,,9999 ");
 
 	}
 
