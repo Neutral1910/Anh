a469f29ed2878d044637fcf0df16148e778c8b6a
Gero Zickermann <hvv@gmx.de>
2021-09-21 15:45:02 +0200

    Signed-off-by: Gero Zickermann <hvv@gmx.de> püp

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World5599999999999999ll999999999 ");
+		System.out.println("Hello World5599999999999999luuuuuuuuuuuuuuuuuuuuuuuuuuul999999999 ");
 
 	}
 
